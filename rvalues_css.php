06bc394eb2da9a6ab16323c1e553c4d1ff6a6bc4
adil192 <adilhanney@gmail.com>
2020-07-08 21:27:47 +0100

    ammended duplicate tags in rvalues_css.php

--- rvalues_css.php
+++ rvalues_css.php
@@ -10,10 +10,6 @@
 		$image="https://adil.hanney.org/repo/previews/rvalues_css.png"
 	);
 	?>
-	<!-- Required meta tags -->
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
 	<!-- Bootstrap CSS -->
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 	
@@ -21,8 +17,6 @@
 	<link rel="stylesheet" href="assets/repo.css">
 	<link rel="stylesheet" href="assets/tests.css">
 	<link rel="stylesheet" href="rvalues.css">
-
-	<title>rvalues.css</title>
 </head>
 
 <body>
