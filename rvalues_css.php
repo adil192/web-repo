5181a5ddfb15b5d6fc324726bce70fdf0f6886b0
adil192 <adilhanney@gmail.com>
2020-07-09 00:57:15 +0100

    updated JQuery version to 3.5.1 non-slim

--- rvalues_css.php
+++ rvalues_css.php
@@ -21,9 +21,8 @@
 
 <body>
 	<!-- these scripts are needed immediately to animate page load -->
-	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+	<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
 	<script src="https://cdn.jsdelivr.net/npm/js-cookie@2/src/js.cookie.min.js"></script>
-	<script src="assets/pageload.js"></script>
 	
 	<?php createHeader("rvalues.css"); ?>
 	
