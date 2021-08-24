7ea87d3b478283233efd86d931e4c77346d71644
Adil Hanney <adilh+github@orchid.anonaddy.com>
2021-08-25 00:58:23 +0100

    global_tools.php: only define page title once

--- index.php
+++ index.php
@@ -22,7 +22,7 @@
 	<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
 	<script src="https://cdn.jsdelivr.net/npm/js-cookie@2/src/js.cookie.min.js"></script>
 	
-	<?php createHeader("Adil's Repo"); ?>
+	<?php createHeader(); ?>
 	
 	<div class="item container">
 		<h3>rvalues.css</h3>
