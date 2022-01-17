e7a3ebab58ad26720c6e1210685ace82ca5e957e
Adil Hanney <adilh+github@orchid.anonaddy.com>
2022-01-17 18:49:14 +0000

    update bootstrap to v5 on various pages

--- rvalues_css.php
+++ rvalues_css.php
@@ -10,10 +10,10 @@
 		$image="https://adil.hanney.org/repo/previews/rvalues_css.png"
 	);
 	?>
-	<!-- Bootstrap CSS -->
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-	
-	<link rel="stylesheet" href="../assets/css/global_styles.css">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="../assets/css/global_styles.css">
 	<link rel="stylesheet" href="assets/repo.css">
 	<link rel="stylesheet" href="assets/tests.css">
 	<link rel="stylesheet" href="rvalues.css">
@@ -375,8 +375,6 @@
 		<a href="#namingscheme" class="btn btn-link">next</a>
 	</div>
 	
-	<script async defer src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-	<script async defer src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 	<script async defer src="assets/script.js" ></script>
 	<script async defer src="../assets/js/autotype.js"></script>
 	<script async defer src="../assets/js/global_scripts.js"></script>
