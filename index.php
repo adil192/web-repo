5ec4434aa667a0fc32ff15d48527f8a44f6c70ba
Adil Hanney <adilh+github@orchid.anonaddy.com>
2022-01-24 02:22:17 +0000

    accessibility: give custom checkboxes the right aria properties

--- index.php
+++ index.php
@@ -19,7 +19,7 @@
 
 <body>
 	<!-- these scripts are needed immediately to animate page load -->
-    <?php enableRangeInputProgressScript(); ?>
+    <?php customThemeScripts(); ?>
 	<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
 	<script src="https://cdn.jsdelivr.net/npm/js-cookie@2.2.1/src/js.cookie.min.js"></script>
 	
