d3701bfc0c754b07c5abda03339eaa81b7fddeb3
Adil Hanney <adilh+github@orchid.anonaddy.com>
2022-01-27 02:52:21 +0000

    rvalues_css.php: fix sliders

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,5 @@
 function itemsRange(slider) {
-	let newClass = "Ximin" + $(slider).val();
+	let newClass = "R Ximin" + $(slider).val();
 	$("#itemsRangeLabel").text("."+newClass);
 	$("div", "#itemsRangeTest").each(function() {
 		let box = $(this);
@@ -7,7 +7,7 @@ function itemsRange(slider) {
 	});
 }
 function percentsRange(slider) {
-	let newClass = "Xpmin" + $(slider).val();
+	let newClass = "R Xpmin" + $(slider).val();
 	$("#percentsRangeLabel").text("."+newClass);
 	$("div", "#percentsRangeTest").each(function() {
 		let box = $(this);
@@ -15,7 +15,7 @@ function percentsRange(slider) {
 	});
 }
 function colsRange(slider) {
-	let newClass = "Xrmin" + $(slider).val();
+	let newClass = "R Xrmin" + $(slider).val();
 	$("#colsRangeLabel").text("."+newClass);
 	$("div", "#colsRangeTest").each(function() {
 		let box = $(this);
