d3f2f03badbdfecd6da3361a1d57126dd75333ee
Adil Hanney <adilh+github@orchid.anonaddy.com>
2022-01-27 18:33:36 +0000

    rvalues_css.php: fix how the slider class is displayed

--- assets/script.js
+++ assets/script.js
@@ -1,24 +1,24 @@
 function itemsRange(slider) {
-	let newClass = "R Ximin" + $(slider).val();
+	let newClass = "Ximin" + $(slider).val();
 	$("#itemsRangeLabel").text("."+newClass);
 	$("div", "#itemsRangeTest").each(function() {
 		let box = $(this);
-		box.removeClass().addClass(newClass).addClass(box.data("color"));
+		box.removeClass().addClass("R " + newClass + box.data("color"));
 	});
 }
 function percentsRange(slider) {
-	let newClass = "R Xpmin" + $(slider).val();
+	let newClass = "Xpmin" + $(slider).val();
 	$("#percentsRangeLabel").text("."+newClass);
 	$("div", "#percentsRangeTest").each(function() {
 		let box = $(this);
-		box.removeClass().addClass(newClass).addClass(box.data("color"));
+		box.removeClass().addClass("R " + newClass + " " + box.data("color"));
 	});
 }
 function colsRange(slider) {
-	let newClass = "R Xrmin" + $(slider).val();
+	let newClass = "Xrmin" + $(slider).val();
 	$("#colsRangeLabel").text("."+newClass);
 	$("div", "#colsRangeTest").each(function() {
 		let box = $(this);
-		box.removeClass().addClass(newClass).addClass(box.data("color"));
+		box.removeClass().addClass("R " + newClass + box.data("color"));
 	});
 }
