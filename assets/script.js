d45946414b09d7e75cb183272fd048c72041451b
Adil Hanney <adilh+github@orchid.anonaddy.com>
2022-01-27 02:50:39 +0000

    rvalues_css.php: remove defunct code

--- assets/script.js
+++ assets/script.js
@@ -1,33 +1,3 @@
-/*$(function() {
-	// START circle transition to new link on <a> click
-	$("a.transitionlink").each(function() {
-		var thislink = $(this);
-		var thiscircle = $(".transitioncircle", this);
-
-		thislink.on('click', function(e) {
-			e.preventDefault();
-			//var new_diameter = Math.max(window.outerWidth, window.outerHeight) * 2;
-			var new_diameter = Math.sqrt(Math.pow(window.outerWidth,2) + Math.pow(window.outerHeight,2)) * 2; // pythagoras
-			console.log(new_diameter);
-			thiscircle.css("--zoomed-diameter", new_diameter+"px");
-			thiscircle.css("animation", "1s zoomIn");
-			thiscircle.css("z-index", 50).css("width", new_diameter).css("height", new_diameter);
-			$("html, body").css("overflow", "hidden");
-			setTimeout(function() {
-				var offset = thislink.offset();
-				var w = thislink.width(), h = thislink.height();
-				console.log(w, h);
-				Cookies.set('o_top', offset.top + h/2, { expires: 1 });
-				Cookies.set('o_left', offset.left + w/2, { expires: 1 });
-				Cookies.set('diameter', new_diameter, { expires: 1 });
-				window.location.href = thislink.attr("href");
-			}, 1000);
-		});
-	});
-	// END circle transition to new link on <a> click
-});*/
-
-
 function itemsRange(slider) {
 	var newclass = "Ximin" + $(slider).val();
 	$("#itemsRangeLabel").text("."+newclass);
